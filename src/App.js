f09ad4dee2e9e114dae9a9634ac9981ed0013989
Abhijeet Prasad <abhi.prasad16@gmail.com>
2022-02-10 11:03:56 +0530

    header component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 
 // Components
-import { Navbar, Header } from './components';
+import { Navbar, Header } from './components'; //by default index.js
 
 function App() {
   return (
-    <div className="app_header">
-      <h1>Hello World </h1>
-    </div>
+    <Header/>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import './header.css';
+function Header() {
+    return (
+      <div className="app_header">
+        <h1>Hello World </h1>
+      </div>
+    );
+}
+
+export default Header;
